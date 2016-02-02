1af98c3468ce6d881be09b0ec6a9415f6401033b
Lucas Neves <lcneves@gmail.com>
2016-02-02 03:42:43 +0200

    Submit picture front-end ready

--- public/script/angular-pin.js
+++ public/script/angular-pin.js
@@ -1,5 +1,27 @@
 (function() {
     var app = angular.module('pinModule', ['loginModule']);
+    
+
+    
+    // Function to replace broken links with a placeholder, including in the preview.
+    // Got it from here: http://stackoverflow.com/questions/16310298/if-a-ngsrc-path-resolves-to-a-404-is-there-a-way-to-fallback-to-a-default\
+    app.directive('errSrc', function() {
+        return {
+            link: function(scope, element, attrs) {
+                element.bind('error', function() {
+                    if (attrs.src != attrs.errSrc) {
+                        attrs.$set('src', attrs.errSrc);
+                    }
+                });
+                attrs.$observe('ngSrc', function(value) {
+                    if (!value && attrs.errSrc) {
+                        attrs.$set('src', attrs.errSrc);
+                    }
+                });
+            }
+        }
+    });
+    
     app.controller('MainController', ['$scope', 'loginStatus', function($scope, loginStatus) {
         
         // Listen to angular-login.js' $emit info on login
@@ -20,6 +42,21 @@
         
         // Begin Your Pin controller logic
         
+        var addObject = {};
+        $scope.submitPicture = function(title, picture) {
+            (function($) {
+                $.post("add-picture", function(data) {
+                    if (data.error) {
+                        $scope.addObject.status = "error";
+                        $scope.addObject.message = data.message;
+                    } else {
+                        $scope.addObject.status = "success";
+                        $scope.addObject.books = data.data;
+                    }
+                    $scope.$apply();
+                });
+            }(jQuery));
+        };
         
         // Initialization
         
